f0f65c486cdd3d79efb6b2650aa1707223362cce
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-12-08 11:40:34 +0300

    pages initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { MainLayout } from './components/MainLayout'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router'
+import { Dashboard } from './pages/Dashboard'
+import { Scales } from './pages/Scales'
+import { Map } from './pages/Map'
+import { Analytics } from './pages/Analytics'
+import { Maintenance } from './pages/Maintenance'
+import { Settings } from './pages/Settings'
 
 
 function App() {
@@ -9,7 +16,19 @@ function App() {
     
     <div>
       <Navbar />
-      <MainLayout />
+      
+      <div>
+        <Routes>
+          <Route path='/' element={<Dashboard /> } />
+          <Route path='/dashboard' element={<Dashboard /> } />
+          <Route path='/scales' element={<Scales /> } />
+          <Route path='/map' element={<Map /> } />
+          <Route path='/analytics' element={<Analytics /> } />
+          <Route path='/maintenance' element={<Maintenance /> } />
+          <Route path='/settings' element={<Settings /> } />
+
+        </Routes>
+      </div>
     </div>
 
     
---
+++ src/pages/Scales.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Scales = () => {
+  return (
+    <div>Scales</div>
+  )
+}
