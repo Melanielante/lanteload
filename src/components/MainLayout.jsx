e7eee85212ff771f4409507464b6bb8ae03f1d30
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-12-08 11:10:38 +0300

    navbar navigation

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+// import { useState } from "react";
+import Navbar from "./components/Navbar";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 
-export const MainLayout = () => {
+export default function Layout() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  // Convert URL path  page ID
+  const currentPage = pathname.replace("/", "") || "dashboard";
+
+  function handleNavigate(page) {
+    navigate(`/${page}`);
+  }
+
   return (
-    <div>MainLayout</div>
-  )
+    <div className="w-full">
+      <Navbar 
+        currentPage={currentPage}
+        onNavigate={handleNavigate}
+      />
+
+      {/* Page Content Area */}
+      <div className="p-6">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
